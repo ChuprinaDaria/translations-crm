ace7e53c46072fc41955a48783ff7a14c26fb1dc
ChuprinaDaria <chuprina.dariia@gmail.com>
2025-11-26 17:56:22 +0100

    Fix CORS: allow all origins temporarily for registration to work

--- app/main.py
+++ app/main.py
@@ -8,17 +8,19 @@ app = FastAPI()
 
 ENV = os.getenv('APP_ENV', 'dev')
 
-if ENV == "dev":
-    allowed = ["*"]
-else:
-    # Production CORS - allow frontend from dzyga-catering.com.ua
-    allowed = [
-        "https://crm.dzyga-catering.com.ua",
-        "http://157.180.36.97",
-        "http://157.180.36.97:8000",
-        "http://157.180.36.97:8080",
-        "https://157.180.36.97:8443"
-    ]
+# Тимчасово дозволяємо всі origins для тесту
+allowed = ["*"]
+# if ENV == "dev":
+#     allowed = ["*"]
+# else:
+#     # Production CORS - allow frontend from dzyga-catering.com.ua
+#     allowed = [
+#         "https://crm.dzyga-catering.com.ua",
+#         "http://157.180.36.97",
+#         "http://157.180.36.97:8000",
+#         "http://157.180.36.97:8080",
+#         "https://157.180.36.97:8443"
+#     ]
 
 app.add_middleware(
     CORSMiddleware,
